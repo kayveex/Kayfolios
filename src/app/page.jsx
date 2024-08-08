b5d99c647158b69a772b9dd7a029ad477d518ad7
unknown <rhesakornelius@gmail.com>
2024-08-08 11:27:29 +0700

    feat: basic config

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-background w-full h-screen">
+
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: "#282C33",
+        primaryYellow: "#FFA841",
+        primaryWhite: "#FFFFFF",
+        secondaryWhite: "#ABB2BF",
+      }
     },
   },
   plugins: [],
